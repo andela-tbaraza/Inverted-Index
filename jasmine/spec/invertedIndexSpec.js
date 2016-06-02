ccfabf951536538410b180b4d0886bfa5ab547e9
Tonida <tonidahnjeli@gmail.com>
2016-06-02 22:43:13 +0300

    implement search Index

--- jasmine/spec/invertedIndexSpec.js
+++ jasmine/spec/invertedIndexSpec.js
@@ -1,10 +1,32 @@
-var f = require("../books.json");
 describe("Read book data", function(){
-  it("check JSON file is not empty", function() {
+  var jsonFile;
+  it("check JSON file is present", function() {
+    fetch("../books.json")
+    .then(function(response){
+        jsonFile = response.json();
+        return jsonFile;
+      })
    //var f = require("../books.json");
-   expect(f).not.toBe(null);
+        expect(jsonFile).not.toBe(null);
   });
+  it("checks if the file is empty", function() {
+    fetch("../books.json")
+    .then(function(reponse){
+      return response.json();
+      // return jsonFile.toString();
+    })
+    .then(function(myblob) {
+      var objectURL = URL.createObjectURL(myBlob);
+    })
+    expect(jsonFile.length).not.toBe(0);
+  })
 });
+
+// describe("populate index", function() {
+//   it("check if the index is populated", function() {
+//     expect(getIndex).not.toBe(null);
+//   });
+// });
 // describe('Tests', function() {
 //   it('tests true', function() {
 //     expect(true).toBe('true');
--- jasmine/src/invertedIndex.js
+++ jasmine/src/invertedIndex.js
@@ -1,20 +1,62 @@
 function Index() {
+  // load books.json file
   this.createIndex = function(filepath) {
-    var f = require(filepath);
-    var r;
-    for (var i = 0; i < f.length; i++) {
-      console.log(JSON.stringify(f[i]).split(""))
-      // for (var key in f[i]) {
-      //   r += f[i][key]
-      // }
+    var jsonFile = require(filepath);
+        // console.log(jsonFile);
+
+  this.getIndex = function() {
+
+    // var arr = [];
+    // jsonFile.forEach(function(objects) {
+    //   (Object.keys(objects)).forEach(function(key) {
+    //     console.log(objects[key].toString().split("-"))
+    //     var str = objects[key].toString();
+    //     // console.log(typeof(str))
+    //     // arr.push(str);
+    //     // console.log(arr)
+    //   });
+    //   });
+
+
+    }
+    this.searchIndex = function(terms){
+    results = [];
+
+  jsonFile.map((book, index) => {
+      const wordToSearch = new RegExp(terms, 'gi');
+      if (wordToSearch.test(book.title) || wordToSearch.test(book.text)) {
+          results.push(index);
+          //console.log(book);
       }
-    return r;
+  });
+
+  if(results.length > 0) {
+      console.log(terms + " has been found in the following documents" + " " + results)
+  } else{
+      console.log("No match has been made")
+  }
+  };
+
+  // searchIndex('lord')
+
+
+    // }z
+    // var r;
+    // for (var i = 0; i < f.length; i++) {
+    //   console.log(JSON.stringify(f[i]).split(" "))
+    //   // for (var key in f[i]) {
+    //   //   r += f[i][key]
+    //   // }
+    //   }
+
     }
   };
 
 
 var obj = new Index()
-console.log(obj.createIndex("../books.json"));
+obj.createIndex("../books.json");
+obj.getIndex()
+obj.searchIndex("and")
 
 
 //console.log(f[0]["title"]);
