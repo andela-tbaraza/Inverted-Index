cdaa61d3f7cdbabc72947688d8d48a62d0b29e4f
Tonida Baraza <tonida.baraza@andela.com>
2016-06-17 02:40:09 +0300

    implement word count

--- jasmine/src/invertedIndex.js
+++ jasmine/src/invertedIndex.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 /*eslint no-unused-vars: "error"*/
 class Index {
   createIndex(filePath) {
@@ -32,20 +32,24 @@ class Index {
     function makeIndex() {
       /* this method creates the index and is to be called
       after the JSON file has been read */
+
       self.books.forEach((book, docIndex) => {
 
+        /* self.bookString is now a string without any special characters and trailing spaces */
         self.bookString = JSON.stringify(book).toLowerCase()
         .replace(/\W/g, ' ').replace(/\s+/g, ' ')
         .trim();
 
-        self.stringArray = self.bookString.split(' ');
+        /* self.stringArray is converted to a set to remove any duplication of words */
+        self.stringArray = new Set((self.bookString.split(' ')));
 
         self.stringArray.forEach((word, wordIndex) => {
-          if (self.indexObject.hasOwnProperty(word)) {
-            self.indexObject[word].push(docIndex)
+          if (self.indexObject.hasOwnProperty(word) === false){
+            self.indexObject[word] = [docIndex]
 
           } else {
-            self.indexObject[word] = [docIndex]
+            self.indexObject[word].push(docIndex)
+            
           }
 
         });
@@ -56,10 +60,34 @@ class Index {
   }
 
   getIndex() {
+    /* returns the index created by createIndex method*/
+
     return this.indexObject
   };
 
+  getFrequency() {
+    /* this method checks how many times a word occurs in the contents JSON file
+    and returns an object with the count */
+    this.frequencyObject = {};
+
+    this.frequencyArray = this.bookString.split(' ');
+
+    this.frequencyArray.map(word => {
+      if (this.frequencyObject.hasOwnProperty(word) === false) {
+        this.frequencyObject[word] = 1
+
+      } else {
+        this.frequencyObject[word] += 1
+      }    
+
+    });
+    return this.frequencyObject
+
+  }
+
   searchIndex(wordInput) {
+    /* depending on the input provided this method will return an object specifying the
+    location of the word in the JSON file contents */
     if (typeof wordInput === 'string') {
       const wordToSearch = wordInput.toLowerCase();
 
