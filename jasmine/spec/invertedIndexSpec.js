94e29bd35c6b0b13c40db24129522b7d3cf89abd
Tonida Baraza <tonida.baraza@andela.com>
2016-06-21 12:08:49 +0300

    refine code

--- jasmine/spec/invertedIndexSpec.js
+++ jasmine/spec/invertedIndexSpec.js
@@ -2,35 +2,32 @@
 
 const index = new Index();
 
-beforeEach((done => {
+beforeEach(done => {
   index.createIndex('books.json').then(() => {
     done();
   });
-})
+});
 
-describe('Index', function () {
-  it('should read the JSON file and assert it is not empty', function () {
+describe('Index', () => {
+  it('should read the JSON file and assert it is not empty', () => {
     expect(index.books.length).toBeGreaterThan(0);
-
   });
 });
 
-describe('populate Index', function () {
-
-  it('should check that index is created once the JSON file has been read', function () {
-    console.log(index.indexObject);
+describe('populate Index', () => {
+  it('should check that index is created once the JSON file has been read', () => {
     expect(index.indexObject.length).not.toBe(0);
   });
 
-  it('should check index maps the string keys to the correct objects in the JSON array.', function() {
+  it('should check index maps the string keys to the correct objects in the JSON array.', () => {
     index.getIndex();
     expect(index.indexObject.powerful).toEqual([1]);
     expect(index.indexObject.falls).toEqual([0]);
   });
 });
 
-describe('Search index', function () {
-  it('searches for terms in a given array', function() {
+describe('Search index', () => {
+  it('searches for terms in a given array', () => {
     expect(index.searchIndex(['Alice', 'lord', 'falls', 'hole', 'and'])).toEqual({
       alice: [0],
       lord: [0, 1],
@@ -40,12 +37,11 @@ describe('Search index', function () {
     });
   });
 
-  it('searches a term and return its location in the JSON', function() {
+  it('searches a term and return its location in the JSON', () => {
     expect(index.searchIndex('Alice')).toEqual([0]);
   });
 
-  it('search a term and return all the instances of that word in the index', function() {
+  it('search a term and return all the instances of that word in the index', () => {
     expect(index.searchIndex('and')).toEqual([0, 1]);
-
   });
 });
--- jasmine/src/invertedIndex.js
+++ jasmine/src/invertedIndex.js
@@ -1,68 +1,67 @@
-'use strict';
-
 class Index {
-  createIndex(filePath) {
-    var self = this;
-    this.indexObject = {};
 
+  makeIndex(books) {
+    /* this method creates the index and is to be called
+    after the JSON file has been read */
+    this.indexObject = {};
+    books.forEach((book, docIndex) => {
+      /* self.bookString is now a string without any special characters and trailing spaces */
+      this.bookString = JSON.stringify(book).toLowerCase()
+        .replace(/\W/g, ' ')
+        .replace(/\s+/g, ' ')
+        .trim();
+
+      /* self.stringArray is converted to a set to remove any duplication of words */
+      this.stringArray = new Set((this.bookString.split(' ')));
+
+      this.stringArray.forEach(word => {
+        if (!this.indexObject.hasOwnProperty(word)) {
+          this.indexObject[word] = [docIndex];
+        } else {
+          this.indexObject[word].push(docIndex);
+        }
+      });
+    });
+  }
+
+  createIndex(filePath) {
     /* specifies the environment to be used */
-    if (typeof window === 'object') {
-      return fetch(filePath).then(function(response) {
-          return response.json();
+    return new Promise((resolve, reject) => {
+      if (typeof window === 'object') {
+        return fetch(filePath)
+        .then(response => response.json())
+        .then(data => {
+          this.books = data;
+          this.makeIndex(data);
+          return resolve();
         })
-        .then(function(data) {
-          self.books = data;
-          makeIndex();
-        });
-    } else {
+        .catch((error) => reject(error));
+      }
       // import the  json file using require
 
-      self.books = require(filePath);
-
-      makeIndex();
-    }
-
-    function makeIndex() {
-
-      /* this method creates the index and is to be called
-      after the JSON file has been read */
-      self.books.forEach((book, docIndex) => {
-
-        /* self.bookString is now a string without any special characters and trailing spaces */
-        self.bookString = JSON.stringify(book).toLowerCase()
-          .replace(/\W/g, ' ')
-          .replace(/\s+/g, ' ')
-          .trim();
-
-        /* self.stringArray is converted to a set to remove any duplication of words */
-        self.stringArray = new Set((self.bookString.split(' ')));
-
-        self.stringArray.forEach((word) => {
-          if (!self.indexObject.hasOwnProperty(word)) {
-            self.indexObject[word] = [docIndex];
-          } else {
-            self.indexObject[word].push(docIndex);
-          }
-        });
-      });
-    }
+      try {
+        const books = require(filePath); // eslint-disable-line global-require
+        this.makeIndex(books);
+        return resolve();
+      } catch (error) {
+        return reject(error);
+      }
+    });
   }
 
   getIndex() {
-
     /* returns the index created by createIndex method*/
     return this.indexObject;
   }
 
   getFrequency() {
-
     /* this method checks how many times a word occurs in the contents JSON file
     and returns an object with the count */
     const frequencyObject = {};
 
     const frequencyArray = this.bookString.split(' ');
 
-    frequencyArray.map(word => {
+    frequencyArray.forEach(word => {
       if (!frequencyObject.hasOwnProperty(word)) {
         frequencyObject[word] = 1;
       } else {
@@ -87,7 +86,7 @@ class Index {
     if (Array.isArray(wordInput) === true) {
       const resultsObject = {};
 
-      wordInput.map(word => {
+      wordInput.forEach(word => {
         const wordToSearch = word.toLowerCase();
 
         if (this.indexObject.hasOwnProperty(wordToSearch)) {
